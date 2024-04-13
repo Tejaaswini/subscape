7a3ec8e0e9da62f9613b0b52d093e7a8b0719afe
tejaaswini <tejnaren07@gmail.com>
2024-04-13 01:18:36 -0400

    add registraion form, pie chart

--- popup/popup.js
+++ popup/popup.js
@@ -254,22 +254,9 @@ document.addEventListener("DOMContentLoaded", function() {
 });
 
 document.addEventListener("DOMContentLoaded", function () {
-  const loginTab = document.getElementById("login-tab");
-  const registerTab = document.getElementById("register-tab");
-  const loginForm = document.querySelector(".login-form");
-  const registerForm = document.querySelector(".register-form");
+  const loginButton = document.querySelector('.new-here');
 
-  loginTab.addEventListener("click", function () {
-    loginTab.classList.add("active");
-    registerTab.classList.remove("active");
-    loginForm.classList.add("active");
-    registerForm.classList.remove("active");
-  });
-
-  registerTab.addEventListener("click", function () {
-    registerTab.classList.add("active");
-    loginTab.classList.remove("active");
-    registerForm.classList.add("active");
-    loginForm.classList.remove("active");
+  loginButton.addEventListener('click', function () {
+    window.location.href = 'registration.html';
   });
 });
